0b8aec3a4bdf6b898d346bd4ecaccdff699fa302
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-11-16 21:08:06 +0800

    refator spot extensions

--- src/RichillCapital.Binance/Spot/BinanceSpotExtensions.cs
+++ src/RichillCapital.Binance/Spot/BinanceSpotExtensions.cs
@@ -15,14 +15,18 @@ public static class BinanceSpotExtensions
         "https://api4.binance.com",
     ];
 
+    private const string SecretKey = "BPwSSG45zE8ABiZ6Zm4t9gJFJMo19ExjBqOQlmLcOM5LgfyYP6V5biYrsUkZfXxm";
+
+    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(5);
+
     public static IServiceCollection AddBinanceSpot(this IServiceCollection services)
     {
         services.TryAddTransient<HttpResponseHandler>();
 
-        services.AddHttpClient<IBinanceSpotRestClient, BinanceSpotRestClient>(client =>
+        services.AddHttpClient<IBinanceSpotRestClient, BinanceSpotRestClient>((serviceProvider, client) =>
         {
             client.BaseAddress = new Uri("https://api.binance.com");
-            client.Timeout = TimeSpan.FromSeconds(5);
+            client.Timeout = DefaultTimeout;
         });
 
         return services;
