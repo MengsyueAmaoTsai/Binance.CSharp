665d1882ecb881617f158a520108f7f24bb2724f
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-11-16 12:34:31 +0800

    add package

---
+++ src/RichillCapital.Binance/Spot/BinanceSpotExtensions.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RichillCapital.Binance.Spot;
+
+public static class BinanceSpotExtensions
+{
+    public static IServiceCollection AddBinanceSpot(this IServiceCollection services)
+    {
+        return services;
+    }
+}
